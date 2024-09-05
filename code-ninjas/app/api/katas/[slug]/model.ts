b14e932ce07f0cf55412d79a0d989169735a27de
Garyismatic <garyevans1501@gmail.com>
2024-09-05 13:41:39 +0100

    added a readme

--- code-ninjas/app/api/katas/[slug]/model.ts
+++
@@ -1,6 +0,0 @@
-import { client, db } from '../../../mongodb/connection'
-
-export async function fetchKata(slug: String) {
-    await client.connect();
-    return db.collection("katas").findOne({ slug });
-  }
--- code-ninjas/app/utils/passedTestPatch.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default async function patchUser(slug: string, username: string) {
-  const { data } = await axios.get(`/api/users/${username}`);
-  if (!data.problems_solved.includes(slug)) {
-    data.problems_solved.push(slug);
-    axios.patch(`/api/users/${username}`, { ...data });
-  }
-}
